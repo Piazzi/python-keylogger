40e8b84e74682fae2cb14788e2914cdabfcf360c
Lucas Piazzi <lpiazzi26@gmail.com>
2021-03-13 16:56:29 -0300

    adds error message on anti spyware

--- anti-spyware.py
+++ anti-spyware.py
@@ -2,10 +2,11 @@ from win32com.client import GetObject
 import subprocess
 import os
 import sys
-print(sys.argv[0][:-3])
+import win32api
 
+print(sys.argv[0][:-3])
 pid = str(os.getpid())
-print("Spyware PID: " + pid)
+print("Anti Spyware PID: " + pid+ "\n")
  
 # traverse the software list
 Data = subprocess.check_output(['wmic', 'process', 'list', 'brief'])
@@ -20,6 +21,7 @@ try:
     for i in range(len(processes)):
         process = arrange_string(processes)[i]
         if process.find("py") > 0 and process.find(pid) == -1:
+            win32api.MessageBox(0, 'The following processes may be a spyware: \nHandle Name Priority ProcessId ThreadCount WorkingSetSize\n'+process , 'Warning!')
             print("\n------------- WARNING ------------- ")
             warning_count += 1
         print(process)

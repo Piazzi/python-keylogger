72754903616167f1fe3d371da30421198f7488aa
Lucas Piazzi <lpiazzi26@gmail.com>
2021-03-13 16:19:55 -0300

    adds new anti spyware

--- anti-spyware.py
+++ anti-spyware.py
@@ -1,19 +1,32 @@
-import ctypes  # An included library with Python install.   
-
+from win32com.client import GetObject
 import subprocess
+import os
+import sys
+print(sys.argv[0][:-3])
 
-cmd = 'WMIC PROCESS get Caption,Commandline,Processid'
-proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+pid = str(os.getpid())
+print("Spyware PID: " + pid)
+ 
+# traverse the software list
+Data = subprocess.check_output(['wmic', 'process', 'list', 'brief'])
+processes = str(Data)
 
-def clean_data(line):
-    return str(line).replace(' ', '')
+#  arrange the string
+def arrange_string(string):
+    return string.split("\\r\\r\\n")
 
-print("-------------------- Running processes --------------------")
-for line in proc.stdout:
-    line = clean_data(line)
-    if(str(line).find(".py") != -1):
-        print('\n ------------ WARNING- -----------  \n'+line+'\n ------------ WARNING- ----------- \n')
-        ctypes.windll.user32.MessageBoxW(0, "Someone may be trying to steal your data. Check the following file: \n"+line, "Warning!", 1)
-    else:
-        print(clean_data(line))
+warning_count = 0
+try:
+    for i in range(len(processes)):
+        process = arrange_string(processes)[i]
+        if process.find("py") > 0 and process.find(pid) == -1:
+            print("\n------------- WARNING ------------- ")
+            warning_count += 1
+        print(process)
+        if(warning_count > 0):
+             print("------------- WARNING ------------- \n")
+        warning_count = 0
+
+except IndexError as e:
+    print("All Done")
 
